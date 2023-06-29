7e14adcf176cac43eb8a2c8154c07cb6e894764b
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-06-29 16:13:19 +0200

    add header and style for header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <script>
+import AppHeader from './components/AppHeader.vue'
 
+export default {
+    components: { AppHeader, },
+    data() {
+        return {
+
+        }
+    },
+}
 </script>
 
-<template></template>
+<template>
+    <AppHeader />
+</template>
 
-<style></style>
+<style lang="scss">
+@use './assets/scss/style.scss'
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,111 @@
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false,
+                },
+            ],
+
+        }
+
+    },
+}
+</script>
+
+<template>
+    <header class="container">
+        <figure>
+            <img src="../assets/img/dc-logo.png" alt="">
+        </figure>
+        <nav>
+            <ul>
+                <li v-for="link in links ">
+                    <a :href="link.url" :key="link.text" :class="{ active: link.current }">{{ link.text }}</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100px;
+
+    figure {
+        width: 80px;
+    }
+}
+
+ul {
+    display: flex;
+
+    li {
+        margin: 0 15px;
+
+        a {
+            color: black;
+            font-weight: 600;
+            text-transform: uppercase;
+            font-size: 0.9rem;
+            padding-bottom: 37px;
+        }
+    }
+}
+
+.active {
+    color: #0282f9;
+    border-bottom: 3px solid #0282f9;
+}
+</style>
